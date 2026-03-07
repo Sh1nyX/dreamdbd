2afc03911f7139fdd175eb0464ea1f015541ae91
Sh1nyX <contrzone2020@gmail.com>
2026-03-07 17:13:08 +0200

    kaif

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const ratingBtn = document.getElementById("ratingShowcaseBtn");
+const killerlistBtn = document.getElementById("killerlistShowcaseBtn");
 
 ratingBtn.addEventListener("click", () =>
 {
     window.location.href = "ratingInfo/ratingInfo.html";
 });
+
+killerlistBtn.addEventListener("click", () =>
+{
+    window.location.href = "dreamdbd/killerlist/killerlist.html";
+})
